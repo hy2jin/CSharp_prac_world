04304551e2a3bc1a4339a05c4f906ff44ea7d0eb
hyejin <hy2jinl22@gmail.com>
2024-03-18 17:51:41 +0900

    DB SELECT 성공

--- WpfApp1/DATABASE/ImDB.cs
+++ WpfApp1/DATABASE/ImDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,20 +18,22 @@ namespace WpfApp1.DATABASE
 
         string _connectionsAddress = "";
 
-        public MySqlConnection Connection { get; private set; } // MySqlConnection 인스턴스를 외부에서 접근할 수 있도록 함
+        public MySqlConnection conn { get; private set; } // MySqlConnection 인스턴스를 외부에서 접근할 수 있도록 함
 
         public ImDB()
         {
             _connectionsAddress = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4}", _server, _port, _database, _id, _pw);
 
             // MySqlConnection 인스턴스 생성
-            Connection = new MySqlConnection(_connectionsAddress);
+            conn = new MySqlConnection(_connectionsAddress);
 
             try
             {
                 // 데이터베이스 연결
-                Connection.Open();
+                conn.Open();
                 Console.WriteLine("연결 성공!");
+
+                SelectData("Name", "city");
             }
             catch (MySqlException ex)
             {
@@ -38,5 +41,48 @@ namespace WpfApp1.DATABASE
             }
         }
 
+        public void SelectData(string sel, string tb, string wh)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                string sql = string.Format("SELECT {0} FROM {1} WHERE {2};", sel, tb, wh);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                adapter.Fill(ds, "members");
+                if (ds.Tables.Count > 0)
+                {
+                    foreach(DataRow dr in ds.Tables[0].Rows)
+                    {
+                        Console.WriteLine(dr["name"]);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        public void SelectData(string sel, string tb)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                string sql = string.Format("SELECT {0} FROM {1};", sel, tb);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                adapter.Fill(ds, "members");
+                if (ds.Tables.Count > 0)
+                {
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        Console.WriteLine(dr["name"]);
+                    }
+                }
+                int bac = 23;
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
